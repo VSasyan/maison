b624c52fafee187d94b0a963196164f8b791a920
Hanane ELJABIRI <hanane.eljabiri@gmail.com>
2015-11-07 17:24:21 +0100

    chart qui marche pas

--- src/calculator.js
+++ src/calculator.js
@@ -1,7 +1,7 @@
 var marker = false; ////Has the user plotted their location marker? 
  var lat;
  var lng;
- 
+ var data;
  function initialize() {
 
   /*map = new google.maps.Map(document.getElementById("map_canvas"), {
@@ -38,6 +38,36 @@ var marker = false; ////Has the user plotted their location marker?
         }
         //Get the marker's location.
         markerLocation();
+		//appel au php
+		$.ajax({
+			url: "position.json.php?lat="+lat+"&lon="+lng,
+			success: function (resp) {
+				 data_response = JSON.parse(resp);
+				 console.log(resp,data_response.data);
+				 $('#ajax').html(data_response.html);
+				 google.load("visualization", "1", {packages:["corechart"]});
+				 google.setOnLoadCallback(draw);
+				 function draw(){
+					 var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Work',     11],
+          ['Eat',      2],
+          ['Commute',  2],
+          ['Watch TV', 2],
+          ['Sleep',    7]
+        ]);
+
+        var options = {
+          title: 'My Daily Activities',
+          pieHole: 0.4,
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+        chart.draw(data, options);
+				 }
+				
+			}  
+		});
     });
 
 
