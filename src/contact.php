2759d41b263cb0e76130c6e796e19b53aed91a97
VSasyan <valentin.sasyan@gmail.com>
2015-11-08 14:59:06 +0100

    contact v2

--- src/contact.php
+++ src/contact.php
@@ -1,15 +1,15 @@
 <?php
 
 $eleves = array(
-	array('De Weyer', 'Dimitri', 'img/perso/no.jpg', '', '', ''),
-	array('Sasyan', 'Valentin', 'img/perso/no.jpg', 'LinkedIn', 'https://www.linkedin.com/in/vsasyan', 'Etudiant à l\'Ecole des Siences Géographiques, Valentin Sasyan est spécialisé en Technique des Systèmes d\'Information.'),
-	array('Eljabiri', 'Hanane', 'img/perso/no.jpg', '', '', ''),
-	array('Mohamed Cassim', 'Adam', 'img/perso/no.jpg', '', '', ''),
-	array('Mougammadoussane', 'Vincent', 'img/perso/no.jpg', '', '', ''),
-	array('Bruyat', 'Agathe', 'img/perso/no.jpg', '', '', ''),
-	array('Briend', 'Mathilde', 'img/perso/no.jpg', '', '', ''),
-	array('Rolland', 'Donatien', 'img/perso/no.jpg', '', '', ''),
-	array('Gansonré', 'Ahmed', 'img/perso/no.jpg', '', '', '')
+	array('De Weyer', 'Dimitri', '', 'LinkedIn', 'https://be.linkedin.com/pub/dimitri-de-weyer/1b/757/6b0', 'Étudiant en Master 2 développement durable, management environnental et géomatique à l\'Université Panthéon Sorbonne.'),
+	array('Sasyan', 'Valentin', 'img/perso/valentin.jpg', 'LinkedIn', 'https://www.linkedin.com/in/vsasyan', 'Etudiant à l\'Ecole des Siences Géographiques, Valentin Sasyan est spécialisé en Technique des Systèmes d\'Information.'),
+	array('Eljabiri', 'Hanane', 'img/perso/hanane.jpg', 'LinkedIn', 'https://www.linkedin.com/in/hananeeljabiri', "Étudiant en Master 2 développement durable, management environnental et géomatique à l'Université Panthéon Sorbonne"),
+	array('Mohamed Cassim', 'Adam', 'img/perso/adam.jpg', 'LinkedIn', 'https://fr.linkedin.com/pub/mohamed-cassim-adam/109/9bb/4b0', "Etudiant en Master de droit mention Développement durable, management environnemental et géomatique à l'Université Panthéon Sorbonne"),
+	array('Mougammadoussane', 'Vincent', '', 'LinkedIn', '', ""),
+	array('Bruyat', 'Agathe', '', 'LinkedIn', '', "Après l'obtention d'un BTS MUC en 2014, j'ai suivi un double cursus : une classe préparatoire CPGE pour les BAC+2 (classe préparatoire ATS) et une licence 3 Economique et Sociale option RH à l'université Panthéon-Sorbonne. Depuis Septembre 2015 je suis étudiante en Master 1 à Novancia Business School (majeure entreprendriat)."),
+	array('Briend', 'Mathilde', 'img/perso/mathilde.jpg', 'LinkedIn', 'https://fr.linkedin.com/pub/mathilde-briend/92/152/bb9', "'A la suite d'un BTS communication, Mathilde a intégré le bachelor de Novancia Business School et suit ce cursus en apprentissage."),
+	array('Rolland', 'Donatien', '', 'LinkedIn', '', ""),
+	array('Gansonré', 'Ahmed', '', 'LinkedIn', '', "")
 );
 
 ?>
@@ -28,7 +28,7 @@ $eleves = array(
 		<script type="text/javascript">
 			$(document).ready(function() {
 				$('.perso').click(function() {
-					window.location.href = $(this).find('a').prop('href');
+					if ($(this).find('a').length > 0) {window.location.href = $(this).find('a').prop('href');}
 				});
 			});
 		</script>
@@ -55,10 +55,10 @@ $eleves = array(
 					if ($type == 'd') {$type = 'g';} else {$type = 'd';}
 					$html .= '<div class="perso ' . $type . '">';
 						$html .= '<h3>' . $eleve[1] . ' ' . $eleve[0] . '</h3>';
-						$html .= '<img src="' . $eleve[2] . '">';
+						if ($eleve[2] != '') {$html .= '<img src="' . $eleve[2] . '">';}
 						$html .= '<div class="info">';
 							$html .= '<p class="desc">' . $eleve[5] . '</p>';
-							$html .= '<p class="contact"><a href="' . $eleve[4] . '" target="_blank">' . $eleve[3] . '</a></p>';
+							if ($eleve[4] != '') {$html .= '<p class="contact"><a href="' . $eleve[4] . '" target="_blank">' . $eleve[3] . '</a></p>';}
 						$html .= '</div>';
 						$html .= '<div style="clear: both"></div>';
 					$html .= '</div>';
